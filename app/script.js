51d671f2135ff864e4c2ba329cc9b9051248d3fe
thcheetah777 <thcheetah777@gmail.com>
2022-03-30 17:50:54 -0700

    Init

---
+++ app/script.js
@@ -0,0 +1,20 @@
+// Dance Clicker
+/*^*^*^*^*^*^*^*
+script.js
+The main script for Dance Clicker.
+*^*^*^*^*^*^*^*/
+
+class Game extends Phaser.Scene {
+  constructor() {
+    super();
+  }
+  preload() {
+
+  }
+  create() {
+
+  }
+  update() {
+
+  }
+}
